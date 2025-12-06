81a1faf5e4ee8f8f65a9acd48e0892d7de49e497
Sinthuha-n <nadasinthu09@gmail.com>
2025-12-06 21:35:16 +0530

    add this

---
+++ src/Thiskey.java
@@ -0,0 +1,37 @@
+class Hum
+{
+    private int age=23;//private is only accessable in same class.
+    private String name;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Thiskey {
+
+        public static void main (String a[])
+        {
+            Hum obj = new Hum();
+            obj.setName("Sinthu");
+            obj.setAge(20);
+
+            System.out.println(obj.getName());
+            System.out.println(obj.getAge());
+        }
+
+
+}
