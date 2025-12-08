57621e01af1ac8e929dd0f6a1bbb8819592d1227
Sinthuha-n <nadasinthu09@gmail.com>
2025-12-08 20:51:44 +0530

    enhanced constructor

--- src/constructor.java
+++ src/constructor.java
@@ -3,11 +3,17 @@ class Humm
     private int age=23;//private is only accessable in same class.
     private String name;
 
-    // constructor
+    // default constructor
     public Humm()
     {
         age = 12;
         name ="john";
+    }
+    //Parameterized Constructor
+    public Humm(int a,String n)
+    {
+        age = a;
+        name =n;
     }
 
     public int getAge() {
@@ -30,10 +36,13 @@ public class constructor {
     public static void main(String a[])
     {
         Humm obj = new Humm();
+        Humm obj1 = new Humm(18,"joel");
         //obj.setName("Sinthu");
         //obj.setAge(20);
 
         System.out.println(obj.getName());
         System.out.println(obj.getAge());
+
+        System.out.println(obj1.getName() +":" + obj1.getAge() );
     }
 }
